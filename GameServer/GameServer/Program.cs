aa948362297f12c86e5025b5418b07593e2a95c9
chenyixuan <1830221593@qq.com>
2022-09-06 18:35:50 +0800

    init

---
+++ GameServer/GameServer/Program.cs
@@ -0,0 +1,13 @@
+﻿using System;
+
+namespace GameServer
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.Title = "GameServer";
+            Console.ReadKey();
+        }
+    }
+}
