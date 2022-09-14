eb2fa9248f9062e381200dfcbb99c3e613e45253
chenyixuan <1830221593@qq.com>
2022-09-14 19:02:30 +0800

    1

---
+++ GameServer/GameServer/ServerSend.cs
@@ -0,0 +1,23 @@
+﻿namespace GameServer
+{
+    public class ServerSend
+    {
+
+        public static void SendTCPData(int _toClient, Packet _packet)
+        {
+            _packet.WriteLength();
+            // Server.clients
+        }
+        
+        public static void Welcome(int _toClient, string _msg)
+        {
+            using (Packet _packet = new Packet((int)ServerPackets.welcome))
+            {
+                _packet.Write(_msg);
+                _packet.Write(_toClient);
+                
+                SendTCPData(_toClient, _packet);
+            }
+        }
+    }
+}
